f558efd0da3d8e74260d6deb35f01e937dd8ace2
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2024-08-12 17:44:31 +0800

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useSwitchChain, useSendTransaction, useChainId } from "wagmi";
+import { parseEther } from "viem";
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const { chains, switchChain } = useSwitchChain();
+  const { sendTransaction } = useSendTransaction();
+  const chainId = useChainId();
+
+  const handleSwitchChain = () => {
+    switchChain({
+      chainId: 42161,
+    });
+  };
+
+  useEffect(() => {
+    console.log('chainId', chainId);
+  }, [chainId]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,66 +34,19 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <ConnectButton />
-
-        <h1 className={styles.title}>
-          Welcome to <a href="">RainbowKit</a> + <a href="">wagmi</a> +{' '}
-          <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://rainbowkit.com">
-            <h2>RainbowKit Documentation &rarr;</h2>
-            <p>Learn how to customize your wallet connection flow.</p>
-          </a>
-
-          <a className={styles.card} href="https://wagmi.sh">
-            <h2>wagmi Documentation &rarr;</h2>
-            <p>Learn how to interact with Ethereum.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://github.com/rainbow-me/rainbowkit/tree/main/examples"
-          >
-            <h2>RainbowKit Examples &rarr;</h2>
-            <p>Discover boilerplate example RainbowKit projects.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Next.js Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-          >
-            <h2>Next.js Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <button onClick={handleSwitchChain}>Switch Chain</button>
+        <button
+          onClick={() =>
+            sendTransaction({
+              to: "0xd2135CfB216b74109775236E36d4b433F1DF507B",
+              chainId: 42161,
+              value: parseEther("0"),
+            })
+          }
+        >
+          Send transaction
+        </button>
       </main>
-
-      <footer className={styles.footer}>
-        <a href="https://rainbow.me" rel="noopener noreferrer" target="_blank">
-          Made with ❤️ by your frens at 🌈
-        </a>
-      </footer>
     </div>
   );
 };
